32574f7c9441c5daff754509963d59fcb606c16c
Sanjit-Prasad <sanjitpd777@gmail.com>
2021-02-03 14:07:00 +0530

    Added clickable to hash_tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'search/index'
   devise_for :users
   get 'home/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'search' => 'search#index'
 
   root to: 'home#index'
 
